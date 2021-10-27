56bae3ea012109dc04757ab39bb02c8c5c004e18
Willyan Bertolino <willyanhpbertolino@gmail.com>
2021-10-27 08:24:56 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import { values } from './data/ramdonValues';
+
+const getUsedValues = () => {
+  let list = localStorage.getItem('randomValuesUsed');
+  if (list) {
+    return JSON.parse(localStorage.getItem('randomValuesUsed'));
+  } else {
+    return [];
+  }
+};
 
 function App() {
+  const [list, setList] = useState(getUsedValues());
+  const [filtered, setFiltered] = useState([]);
+
+  function filteredFunction(arr1, arr2) {
+    arr1 = arr1.filter((item) => {
+      return !arr2.includes(item);
+    });
+
+    return arr1;
+  }
+
+  useEffect(() => {
+    setFiltered(filteredFunction(values, list));
+    localStorage.setItem('randomValuesUsed', JSON.stringify(list));
+  }, [list]);
+
+  const handleClick = (e, index) => {
+    let item = filtered[index];
+    e.target.disabled = true;
+    navigator.clipboard.writeText(item);
+    alert(`copiado`);
+    setList((prev) => [...prev, item]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {filtered.map((item, index) => {
+        return (
+          <button
+            key={item}
+            className="content"
+            type="button"
+            onClick={(e) => handleClick(e, index)}
+          >
+            <p>{item}</p>
+          </button>
+        );
+      })}
     </div>
   );
 }
